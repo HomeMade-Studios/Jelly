00514b0839277932d526747eb49d71f209cd4a58
Romboide82 <ugo98.juve@libero.it>
2015-05-24 14:24:12 +0200

    start

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movement : MonoBehaviour {
+
+	public CNAbstractController movementController;
+	public float speed = 100;
+
+	void Start () {
+
+	}
+	
+	void Update() {
+		float translationY = movementController.GetAxis("Vertical") * speed * Time.deltaTime;
+		float translationX = movementController.GetAxis("Horizontal") * speed * Time.deltaTime;
+		transform.Translate (translationX, translationY, 0);
+
+	}
+}
---
+++ Assets/Scripts/Scaling.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Scaling : MonoBehaviour {
+
+	public float size;
+
+	void Awake (){
+		size = 10;
+	}
+
+	void Start () {
+
+	}
+
+	void Update () {
+		iTween.ScaleUpdate (this.gameObject, new Vector3 (size, size, 1), 5);
+	}
+}
