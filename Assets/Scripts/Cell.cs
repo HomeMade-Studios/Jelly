8bf46fc11ca5794f68d3b6e28fda1c3822a685e6
Romboide82 <ugo98.juve@libero.it>
2015-06-24 00:36:09 +0200

    c

--- Assets/Scripts/Cell.cs
+++ Assets/Scripts/Cell.cs
@@ -4,12 +4,27 @@ using System.Collections;
 public class Cell : MonoBehaviour {
 
 	public float mass;  	//EQUAL TO RADIUS
+	public float speed;
 	private float scale;	//TRANSFORM.SCALE PARAMETER
 	private float radius;	//RADIUS OF THE CIRCLE
-	private bool eated = false;  //IS ALREADY BE EATED
+	private bool eated = false;
+	private bool justSplitted;
 
 	void Update(){
 		Scale();
+		if(Input.GetKeyDown("space")){
+			Split();
+		}
+
+		//		FOR MOUSE AND FINGER POSITION
+		if (Input.touchSupported) {
+			if (Input.touchCount > 0)
+				transform.position = Vector2.MoveTowards (transform.position, Camera.main.ScreenToWorldPoint (Input.GetTouch (0).position), speed * Time.deltaTime);
+		}
+		else {
+			//transform.position = Vector2.MoveTowards (transform.position, Camera.main.ScreenToWorldPoint (Input.mousePosition), speed * Time.deltaTime);
+			GetComponent<Rigidbody2D>().velocity = (Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position).normalized * speed;
+		}
 	}
 
 	void OnTriggerStay2D(Collider2D other){
@@ -36,6 +51,22 @@ public class Cell : MonoBehaviour {
 		scale = (radius * 1.11f)/2;
 		Vector3 newScale = new Vector3 (scale, scale, 1);
 		transform.localScale = Vector3.Lerp(transform.localScale, newScale, 2.5f*Time.deltaTime);
+	}
+
+	public void ScaleImmediate(){
+		radius = Mathf.Sqrt(mass/Mathf.PI);
+		scale = (radius * 1.11f)/2;
+		Vector3 newScale = new Vector3 (scale, scale, 1);
+		transform.localScale = newScale;
+	}
+
+	public void Split(){
+		Mass /= 2;
+		GameObject splittedCell = Instantiate(Resources.Load ("Prefabs/Player Cell", typeof(GameObject)), transform.position, Quaternion.identity) as GameObject;
+		splittedCell.transform.SetParent(GameObject.Find("Player Cells Container").transform);
+		splittedCell.GetComponent<Cell>().Mass = Mass;
+		splittedCell.GetComponent<Cell>().ScaleImmediate();
+		splittedCell.GetComponent<Rigidbody2D>().AddForce((Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position).normalized * 100 , ForceMode2D.Impulse);	//NEED SPLIT FORCE FORMULA
 	}
 	
 	void Destroy(){
--- Assets/Scripts/LeaderBoard.cs
+++ Assets/Scripts/LeaderBoard.cs
@@ -6,4 +6,14 @@ public class LeaderBoard : MonoBehaviour {
 
 	public Text[] playerNames;
 
+	void update(){
+		updateLeaderPlayer();
+	}
+
+	void updateLeaderPlayer(){
+
+		for (int i = 0; i < playerNames.Length; i++){
+			//playerNames[i].text = ................
+		}
+	}
 }
